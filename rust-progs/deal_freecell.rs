f8ccf2a88dbd787a60bfed257fcd3c20158b1de4
Shlomi Fish <shlomif@shlomifish.org>
2013-06-07 19:57:04 +0300

    Add the program with the compile error.

---
+++ rust-progs/deal_freecell.rs
@@ -0,0 +1,32 @@
+use core::util;
+
+struct MSRand {
+    seed: i32
+}
+
+impl MSRand {
+    fn rand(&mut self) -> i32 {
+        self.seed = ((self.seed * 214013i32 + 2531011i32) & 0x7FFFFFFFi32);
+        return ((self.seed >> 16i32) & 0x7FFFi32);
+    }
+    fn max_rand(&mut self, mymax: i32) -> i32 {
+        return (self.rand() % mymax);
+    }
+    fn shuffle<T>(&mut self, deck: &mut [T]) {
+        if (deck.len() > 0) {
+            let mut i = deck.len() as i32;
+            while (--i > 0) {
+                let j = self.max_rand(i+1i32);
+                core::util::swap((&mut deck[i]), (&mut deck[j]));
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut r = MSRand { seed: 24,};
+
+    println(fmt!("Result=%i",r.rand() as int));
+    // println(fmt!("Result=%i",r.rand()));
+    // println(fmt!("Result=%i",r.rand()));
+}
